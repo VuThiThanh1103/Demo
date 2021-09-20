bc52f38a0c4121bb2cd4a6301ceb31c97222c5ff
VuThiThanh1103 <vuthanh2906@gmail.com>
2021-09-20 11:28:43 +0700

    aa

---
+++ Data/MvcStudentsContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcStudent.Models;
+
+    public class MvcStudentsContext : DbContext
+    {
+        public MvcStudentsContext (DbContextOptions<MvcStudentsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcStudent.Models.Student> Student { get; set; }
+    }
